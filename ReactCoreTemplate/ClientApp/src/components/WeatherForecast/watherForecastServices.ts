19f304a550eb4ad10273aa98710c4d944fb57906
Jiarong Gu <jiaronggu@hotmail.com>
2019-02-21 15:38:21 +1100

    update types

--- ReactCoreTemplate/ClientApp/src/components/WeatherForecast/watherForecastServices.ts
+++ ReactCoreTemplate/ClientApp/src/components/WeatherForecast/watherForecastServices.ts
@@ -2,21 +2,26 @@ import { matchPath } from 'react-router';
 import { ReduxCreator } from '@utils';
 import { WeatherForecastSource } from './WatherForecastSource';
 import { ApplicationState } from '@store';
+import { Store } from 'redux';
 
 export class WatherForecastState {
   forecasts: any[];
 }
 
-const locationHanlder = async (store, payload) => {
-  var matches = matchPath(payload.pathname,  { path: '/weather-forecast/:startDateIndex?'});
+const locationHanlder = async (store: Store<ApplicationState>, location: Location) => {
+  var matches = matchPath(location.pathname,  { path: '/weather-forecast/:startDateIndex?'});
+
   if (matches) {
-    const httpConfig = (store.getState() as ApplicationState).httpConfig.config;
-    const forecasts = await new WeatherForecastSource(httpConfig).fetchdata((matches.params as any).startDateIndex);
+    const startDateIndex = (matches.params as any).startDateIndex;
+    const httpConfig = store.getState().httpConfig.config;
+    const forecasts = await new WeatherForecastSource(httpConfig).fetchdata(startDateIndex);
+
     store.dispatch(watherForecastActions.setForcasts(forecasts));
   }
 };
 
-export const watherForecastActions = new ReduxCreator<WatherForecastState>('watherForecast', new WatherForecastState())
+export const watherForecastActions = 
+  new ReduxCreator<WatherForecastState>('watherForecast', new WatherForecastState())
   .addReducer((state, forecasts) => ({ ...state, forecasts }), 'setForcasts')
   .addLocationHandler(locationHanlder)
   .build();
